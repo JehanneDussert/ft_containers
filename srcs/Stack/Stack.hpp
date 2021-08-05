661546c082ea4163481241c0366cda2efaa31343
Jehanne Dussert <jehanne-dussert@hotmail.fr>
2021-08-05 09:44:37 +0200

    [Stack] Add operator=

--- srcs/Stack/Stack.hpp
+++ srcs/Stack/Stack.hpp
@@ -14,17 +14,17 @@ namespace ft
 			typedef Container		container_type;
 			typedef unsigned int	size_type;
 
-			explicit			stack(const container_type& ctnr = container_type());
-			virtual 			~stack(void) { return ; };
-			bool				empty() const;
-			size_type			size() const;
-			value_type& 		top();
-			const value_type&	top() const;
-			void 				push(const value_type& val);
-			void 				pop();
-			stack				&operator=(const stack& x);
-			friend bool operator==(const stack<T, Container> &lhs, const stack<T, Container> &rhs) { return (lhs._ctnr == rhs._ctnr); };
-			friend bool operator<(const stack<T, Container> &lhs, const stack<T, Container> &rhs) { return (lhs._ctnr < rhs._ctnr); };
+			explicit				stack(const container_type& ctnr = container_type());
+			virtual 				~stack(void) { return ; };
+			bool					empty() const;
+			size_type				size() const;
+			value_type& 			top();
+			const value_type&		top() const;
+			void 					push(const value_type& val);
+			void 					pop();
+			stack					&operator=(const stack& x);
+			friend bool 			operator==(const stack<T, Container> &lhs, const stack<T, Container> &rhs) { return (lhs._ctnr == rhs._ctnr); };
+			friend bool 			operator<(const stack<T, Container> &lhs, const stack<T, Container> &rhs) { return (lhs._ctnr < rhs._ctnr); };
 
 		private:
 			container_type	_ctnr;
